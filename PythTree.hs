389465bbb5bb7702d05ee97f23bdd40b0fd658d5
ToF- <cthibaut@octo.com>
2015-12-31 19:21:21 +0100

    squareAndTriangle

--- PythTree.hs
+++ PythTree.hs
@@ -2,3 +2,11 @@ module PythTree
 where
 
 oppositeAngle  h a = (h * cos a * cos a, h * cos a * sin a)
+
+squareAndTriangle s a = 
+    [(0.0, 0.0)
+    ,(s  , 0.0)
+    ,(s  , s  )
+    ,(x ,  s+y)
+    ,(0.0, s)]
+    where (x,y) = oppositeAngle s a
--- Specs.hs
+++ Specs.hs
@@ -5,6 +5,8 @@ margin = 0.00001
 
 shouldBeApprox (x,y) (x',y') = (rounded x,rounded y) `shouldBe` (x',y')
 
+shouldBeApproxShape pts pts' = map (\(x,y) -> (rounded x, rounded y)) pts `shouldBe` pts'
+
 rounded x = (fromIntegral (round (x*10000)))/ 10000
 
 main = hspec $ do
@@ -13,4 +15,14 @@ main = hspec $ do
             oppositeAngle 1.0 (pi/4)  `shouldBeApprox`  (0.5,0.5)
             oppositeAngle 10.0 (pi/8)  `shouldBeApprox`  (8.5355,3.5355) 
             oppositeAngle 100.0 (pi/3) `shouldBeApprox` (25.0,43.3013)
+
+    describe "square and triangle" $ do 
+        it "given a size and angle, yields a square and rectangle triangle" $ do
+            squareAndTriangle 10.0 (pi/8)  `shouldBeApproxShape` 
+                [(0.0, 0.0)
+                ,(10.0, 0.0)
+                ,(10.0, 10.0)
+                ,(8.5355, 13.5355)
+                ,(0.0, 10.0)]
+
 
