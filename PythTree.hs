2b46e75de5409d7ad81f5240584b68634a11fc49
ToF- <cthibaut@octo.com>
2016-01-01 09:47:16 +0100

    refactoring, with alias types Point, Angle, Size

--- PythTree.hs
+++ PythTree.hs
@@ -1,10 +1,15 @@
 module PythTree
 where
 
+type Size  = Float
+type Angle = Float
 type Point = (Float, Float)
 data Shape = Shape [Point]
+
+oppositeAngle :: Size -> Angle -> Point
 oppositeAngle  h a = (h * cos a * cos a, h * cos a * sin a)
 
+squareAndTriangle :: Size -> Angle -> Shape
 squareAndTriangle s a = 
     Shape [(0.0, 0.0)
           ,(s  , 0.0)
